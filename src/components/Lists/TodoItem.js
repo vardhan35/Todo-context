3131ba051e53d68ec78c65c98b35114af42f8ee2
vardhan <harshwardhankalse03@gmail.com>
2021-06-02 11:40:40 +0530

    Todo-Item: Line Through

--- src/components/Lists/TodoItem.js
+++ src/components/Lists/TodoItem.js
@@ -1,13 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TodoContext } from "../../context/TodoContext";
 
 const TodoItem = ({ todo }) => {
+  const [isComplete, setIsComplete] = useState(false);
   const { removeTodo } = useContext(TodoContext);
+
+  const handleClick = () => {
+    setIsComplete(!isComplete);
+  };
   return (
-    <div key={todo.id}>
+    <div
+      key={todo.id}
+      className={isComplete ? "todo-item-complete" : "todo-item"}
+    >
       <h1>{todo.title}</h1>
       <button type="button" onClick={() => removeTodo(todo.id)}>
         Remove
+      </button>
+      <button type="button" onClick={handleClick}>
+        button
       </button>
     </div>
   );
