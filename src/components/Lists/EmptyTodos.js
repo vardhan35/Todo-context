647dd80c6bc33e3d0970e40ad32e29c27fbf945c
vardhan <harshwardhankalse03@gmail.com>
2021-06-30 19:46:45 +0530

    .

--- src/components/Lists/EmptyTodos.js
+++ src/components/Lists/EmptyTodos.js
@@ -3,7 +3,7 @@ import React from "react";
 const EmptyTodos = () => {
   return (
     <div className="empty_todos">
-      <h1>Youre All Done..!!</h1>
+      <h1>You're All Done..!!</h1>
     </div>
   );
 };
